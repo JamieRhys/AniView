7150a20b1bac45e5057f86e3b7f4b938e4520105
JamieRhys <jamie_airfix@yahoo.co.uk>
2025-07-18 15:05:58 +0100

    Initial Commit

---
+++ AniView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AniView
+//
+//  Created by Jamie-Rhys Edwards on 18/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

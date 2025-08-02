b1c7522037ebb493caaee39cabd34681f93b118c
JamieRhys <jamie_airfix@yahoo.co.uk>
2025-08-02 15:51:03 +0100

    Cleaned up splash feature

--- AniView/Features/Splash/Presentation/View/SplashViewController.swift
+++ AniView/Features/Splash/Presentation/View/SplashViewController.swift
@@ -7,17 +7,21 @@
 
 import UIKit
 import SnapKit
+import RxSwift
 import OSLog
 
 final class SplashViewController: UIViewController {
-    private let viewModel: SplashViewModel
+    private let viewModel: SplashViewModelProtocol
+    private let disposeBag: DisposeBag
     var coordinator: SplashCoordinator?
     
-    private let labelTitle = UILabel.headline(text: NSLocalizedString("label.app_name", comment: "Title of the app"))
+    private let labelTitle = UILabel.headline(text: "")
     private let progressIndicator = UIActivityIndicatorView(style: .large)
+    private var iconTickView = UIImageView()
     
     init(viewModel: SplashViewModel) {
         self.viewModel = viewModel
+        self.disposeBag = DisposeBag()
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -25,13 +29,47 @@ final class SplashViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureBindings()
         setupUi()
         setupConstraints()
-        coordinator?.startSwitchTimer()
     }
     
     override func viewDidDisappear(_ animated: Bool) {
         coordinator?.end()
+    }
+    
+    private func configureBindings() {
+        viewModel
+            .labelTitle
+            .drive(onNext: { [weak self] text in
+                self?.labelTitle.text = text
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel
+            .loadingState
+            .drive(onNext: { [weak self] state in
+                switch state {
+                case LoadingState.loading:
+                    self?.progressIndicator.startAnimating()
+                case LoadingState.loaded:
+                    Logger.statistics.info("Loaded")
+                    self?.progressIndicator.stopAnimating()
+                    self?.progressIndicator.isHidden = true
+                    self?.iconTickView.isHidden = false
+                    self?.coordinator?.startSwitchTimer()
+                default:
+                    Logger.viewcycle.error("Loading state: \(state)")
+                }
+            })
+            .disposed(by: disposeBag)
+        
+        viewModel
+            .iconTick
+            .drive(onNext: { [weak self] systemName in
+                self?.iconTickView.image = UIImage(systemName: systemName)
+            })
+            .disposed(by: disposeBag)
     }
     
     private func setupUi() {
@@ -42,11 +80,14 @@ final class SplashViewController: UIViewController {
         gradient.endPoint = CGPoint(x: 0.75, y: 0.75)
         view.layer.insertSublayer(gradient, at: 0)
         
-        
         labelTitle.textAlignment = .center
+        
+        iconTickView.tintColor = AppColor.textPrimary
+        iconTickView.isHidden = true
         
         view.addSubview(labelTitle)
         view.addSubview(progressIndicator)
+        view.addSubview(iconTickView)
     }
     
     private func setupConstraints() {
@@ -56,6 +97,11 @@ final class SplashViewController: UIViewController {
         }
         
         progressIndicator.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(labelTitle.snp.bottom).offset(16)
+        }
+        
+        iconTickView.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.top.equalTo(labelTitle.snp.bottom).offset(16)
         }
